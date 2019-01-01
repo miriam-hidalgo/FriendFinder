85badc4e060db852168f0f2994116fcda38c7179
Miri.Hid <mirihdz11@gmail.com>
2019-01-01 13:28:22 -0500

    Deleted README & updated compatibility logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,22 +4,29 @@ const friends = require("../data/friends");
 // Displays JSON of all possible friends
 module.exports = function(app){
     app.get("/api/friends", function(req, res) {
-        return res.json(friends);
-        });
+        res.json(friends);
+        console.log(friends);
+    });
 //A POST route `/api/friends`
 //Takes in JSON user survey input
 //Handles incoming survey results & compatibility logic
-    app.post("/api/friends", function(req, res) {
-        console.log(userInput)
+    app.post("/api/friends", function(req, res) {        
+        let userAnswers = req.body
+
+        for(let i = 0; i < friends.length; i++){
+            let totalDifference = 0
+        
+            for(let j = 0; j < friends[i].scores.length; j++){
+                totalDifference += Math.abs(userAnswers.scores[j] - friends[i].scores[j]);
+                } 
+                //totalDifference is an array of the total difference of scores for each friend, with the below code I wanted to grab the minimun score from the array and then alert it
+                    //lowestScore = Math.min(...totalDifference)); 
+                    
+                    //alert("Your matched friend is: " + friends[i].name + "!" + "/n" + friends[i].photo);
+            }       
+                //holding difference of each friend
+                console.log(friends[i].name + ":" + totalDifference);
+                //console.log("Lowest score:" + lowestScore);   
+        friends.push(userAnswers);   
     });
 };
-
-//Compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//Example:
-//User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//Total Difference: **2 + 1 + 2 =** **_5_**
-//Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
-//The closest match will be the user with the least amount of difference.
-//Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-//The modal should display both the name and picture of the closest match.
